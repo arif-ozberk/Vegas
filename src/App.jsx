8f7291a53e9df818727d77c1fe2c0f58ff8707e3
arif-ozberk <ozberkazmak@gmail.com>
2023-09-25 23:25:03 +0300

    move routes to external file

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,21 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
 // Styles
 import './styles/globals.scss';
 
 // Components
-import Navbar from './components/shared_components/Navbar';
 import LoginLoading from './components/shared_components/LoginLoading';
-
-// Pages
-import HomePage from './pages/HomePage';
-import BalancePage from './pages/BalancePage';
-import ComingSoonPage from './pages/ComingSoonPage';
-
-import SlotPage from './pages/SlotPage';
-import RoulettePage from './pages/RoulettePage';
-import CoinFlipPage from './pages/CoinFlipPage';
-import CrashPage from './pages/CrashPage';
 
 // Context
 import { mainContext } from './context/mainContext';
 
 // Mocks
 import { topNotificationOptions, bottomNotificationOptions } from './mocks/NotificationOptions';
+
+// Routes
+import { router } from './routes/routes';
 
 
 function App() {
@@ -35,46 +27,6 @@ function App() {
     const [userBalance, setUserBalance] = useState(1000);
     const [gameNotification, setGameNotification] = useState(true);  // Game notification mute control state
     const [isInfoOn, setIsInfoOn] = useState(false);  // Game info popup control state
-
-    const routes = [
-        {
-            routePath: "/balance-page",
-            routeElement: <BalancePage />
-        },
-        {
-            routePath: "/slot-page",
-            routeElement: <SlotPage />
-        },
-        {
-            routePath: "/roulette-page",
-            routeElement: <RoulettePage />
-        },
-        {
-            routePath: "/coin-flip-page",
-            routeElement: <CoinFlipPage />
-        },
-        {
-            routePath: "/crash-page",
-            routeElement: <CrashPage />
-        },
-        {
-            routePath: "/dice-page",
-            routeElement: <ComingSoonPage />
-        }
-    ];
-
-
-    const router = createBrowserRouter(
-        createRoutesFromElements(
-            <Route path='/' element={<Navbar />} >
-                <Route index element={<HomePage />} />
-
-                {routes.map((route, index) => (
-                    <Route key={route.routePath} path={route.routePath} element={route.routeElement} />
-                ))}
-            </Route>
-        )
-    )
 
 
     useEffect(() => {
---
+++ src/routes/routes.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+
+// Components
+import Navbar from '../components/shared_components/Navbar';
+
+// Pages
+import HomePage from '../pages/HomePage';
+import BalancePage from '../pages/BalancePage';
+import SlotPage from '../pages/SlotPage';
+import RoulettePage from '../pages/RoulettePage';
+import CoinFlipPage from '../pages/CoinFlipPage';
+import CrashPage from '../pages/CrashPage';
+import ComingSoonPage from '../pages/ComingSoonPage';
+
+
+const routes = [
+    {
+        routePath: "/balance-page",
+        routeElement: <BalancePage />
+    },
+    {
+        routePath: "/slot-page",
+        routeElement: <SlotPage />
+    },
+    {
+        routePath: "/roulette-page",
+        routeElement: <RoulettePage />
+    },
+    {
+        routePath: "/coin-flip-page",
+        routeElement: <CoinFlipPage />
+    },
+    {
+        routePath: "/crash-page",
+        routeElement: <CrashPage />
+    },
+    {
+        routePath: "/dice-page",
+        routeElement: <ComingSoonPage />
+    }
+];
+
+
+export const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<Navbar />} >
+            <Route index element={<HomePage />} />
+
+            {routes.map((route, index) => (
+                <Route key={route.routePath} path={route.routePath} element={route.routeElement} />
+            ))}
+        </Route>
+    )
+)
